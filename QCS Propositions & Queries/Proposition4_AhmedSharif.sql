602f7cf400e8b303f5b4e44e26345369aa00b75f
Benjamin Zhong <97631450+benjaminzhong1@users.noreply.github.com>
2024-05-14 11:54:12 -0400

    Create Proposition4_AhmedSharif.sql

---
+++ QCS Propositions & Queries/Proposition4_AhmedSharif.sql
@@ -0,0 +1,13 @@
+-- =============================================
+-- Author:		<Ahmed>
+-- Create date: <5/13/2024>
+-- Description:	<Proposition #4Finds the courses with the highest and lowest enrollments>
+-- =============================================
+SELECT c.CourseCode, 
+       c.Description,
+       MAX(cl.Enrollment) AS MaxEnrollment,
+       MIN(cl.Enrollment) AS MinEnrollment
+FROM Course.Course c
+LEFT JOIN Course.Class cl ON c.CourseID = cl.ClassID
+GROUP BY c.CourseCode, c.Description
+ORDER BY MaxEnrollment DESC, MinEnrollment ASC;
