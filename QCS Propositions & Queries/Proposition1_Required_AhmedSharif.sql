23c68e0bb996734c9484eabeb6c9abae8fd2120d
Benjamin Zhong <97631450+benjaminzhong1@users.noreply.github.com>
2024-05-14 12:01:28 -0400

    Update Proposition1_Required_AhmedSharif_AshishMaharjan.sql

--- QCS Propositions & Queries/Proposition1_Required_AhmedSharif.sql
+++ QCS Propositions & Queries/Proposition1_Required_AhmedSharif.sql
@@ -1,8 +1,8 @@
--- =============================================
+-- =================================================================================================================
 -- Author:		<Ahmed>
 -- Create date: <5/13/2024>
--- Description: Proposition #1 Required - Show all instructors who are teaching in classes in multiple departments
--- =============================================                         
+-- Description: Proposition #1.1 Required - Show all instructors who are teaching in classes in multiple departments
+-- =================================================================================================================
 
 SELECT i.FirstName, i.LastName, COUNT(DISTINCT d.DepartmentName) AS DepartmentCount
 FROM Department.Instructor i
@@ -10,3 +10,23 @@ JOIN Department.InstructorDepartment id ON i.InstructorID = id.InstructorID
 JOIN Department.Departments d ON id.DepartmentID = d.DepartmentID
 GROUP BY i.FirstName, i.LastName
 HAVING COUNT(DISTINCT d.DepartmentName) > 1
+
+
+
+-- =================================================================================================================
+-- Author:		<Ashish>
+-- Create date: <5/13/2024>
+-- Description: Proposition #1.2 Required - Show all instructors who are teaching in classes in multiple departments
+-- =================================================================================================================      
+SELECT
+    i.FirstName,
+    i.LastName,
+    COUNT(DISTINCT d.DepartmentID) AS NumberOfDepartments
+FROM
+    Department.Instructor i
+JOIN Department.InstructorDepartment id ON i.InstructorID = id.InstructorID
+JOIN Department.Departments d ON id.DepartmentID = d.DepartmentID
+GROUP BY
+    i.FirstName, i.LastName
+HAVING
+    COUNT(DISTINCT d.DepartmentID) > 1;
