7edf1a40dd10226e725e6cd52fb6a3c7e27d39dc
Benjamin Zhong <97631450+benjaminzhong1@users.noreply.github.com>
2024-05-14 11:48:40 -0400

    Update Proposition3_Required_RahibKhandaker.sql

--- QCS Propositions & Queries/Proposition3_Required_RahibKhandaker.sql
+++ QCS Propositions & Queries/Proposition3_Required_RahibKhandaker.sql
@@ -1,5 +1,13 @@
--- Proposition #3 Required
--- How may classes that are being taught that semester grouped by course and aggregating the total enrollment, total class limit and the percentage of enrollment.
+-- ====================================================================================================================================================================================================
+-- Author:		<Rahib>
+-- Create date: <5/13/2024>
+-- Description: Proposition #3 Required - How may classes that are being taught that semester grouped by course and aggregating the total enrollment, total class limit and the percentage of enrollment.
+-- ====================================================================================================================================================================================================
+
+
+
+
+
 select CourseCode, count(a.CourseId), sum(Enrollment), sum(Limit), (sum(Enrollment) * 100/ sum(Limit)) from Course.CoursesCLassMode a
 join Course.Class b on a.ClassID = b.Classid
 join Course.Course c on a.CourseId = c.Courseid
