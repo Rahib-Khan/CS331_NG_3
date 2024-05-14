15f5f4b039f12552849a7c0d107e96ff6c2f0205
Benjamin Zhong <97631450+benjaminzhong1@users.noreply.github.com>
2024-05-14 10:34:16 -0400

    Create QCS_DataCleansing_(Ben+Rahib).sql

---
+++ QCS Data Cleansing & Anomalies/QCS_DataCleansing_(Ben and Rahib).sql
@@ -0,0 +1,38 @@
+--Anomalies
+
+
+--Day Anomaly
+USE QueensClassSchedule
+UPDATE Uploadfile.CurrentSemesterCourseOfferings 
+SET Day = 'U'
+WHERE DAY LIKE ''
+
+--Time Anomaly
+USE QueensClassSchedule
+UPDATE Uploadfile.CurrentSemesterCourseOfferings 
+SET Time = '12:00 AM - 12:00 AM'
+WHERE TIME LIKE '-' 
+
+--Blank Instructor Anomaly
+USE QueensClassSchedule
+UPDATE Uploadfile.CurrentSemesterCourseOfferings 
+SET Instructor = 'John, Smith'
+WHERE Instructor LIKE ',' 
+
+--Blank Location Anomaly
+USE QueensClassSchedule
+UPDATE Uploadfile.CurrentSemesterCourseOfferings 
+SET Location = 'Unknown '
+WHERE Location LIKE 'Unknown' 
+
+
+
+--Limit Exceeded Anomaly
+-- Change limit and enrolled columns into int
+USE QueensClassSchedule
+UPDATE Uploadfile.CurrentSemesterCourseOfferings 
+SET Limit = Enrolled + 10
+WHERE Enrolled > Limit
+
+
+select * from Uploadfile.CurrentSemesterCourseOfferings 
