410fe7f6b0f4b0000417fb279c8bd00663f9e95d
Benjamin Zhong <97631450+benjaminzhong1@users.noreply.github.com>
2024-05-14 11:44:53 -0400

    Create Proposition#2_Required_Ben_Zhong+Rahib_Khandaker.sql

---
+++ QCS Propositions & Queries/Proposition#2_Required_Ben_Zhong+Rahib_Khandaker.sql
@@ -0,0 +1,10 @@
+-- =============================================
+-- Author:		<Ashish>
+-- Create date: <5/13/2024>
+-- Description:	Proposition #2 Required - How many instructors are in each department?
+-- =============================================
+select DepartmentName, count(b.InstructorId) from Department.Departments a
+join Department.InstructorDepartment b  on a.DepartmentID = b.DepartmentID
+join Department.Instructor c on c.InstructorID = b.InstructorId
+where b.InstructorID <> 676
+group by DepartmentName
