2589ca41e060e4de18fb74077e9d6ec3338b3fee
benjaminzhong1 <benjaminzhong1@gmail.com>
2024-05-14 18:40:33 -0400

    update DropFKs on individial folders esteban and rahib

--- Individual Member Work/Esteban Mesa/[Drop_ForeignKeys].sql
+++ Individual Member Work/Esteban Mesa/[Drop_ForeignKeys].sql
@@ -3,65 +3,48 @@
 -- Author: Esteban Mesa & Rahib Khandaker
 -- Procedure: [Project3].[DropForeignKeysFromQueensClassSchedule]
 -- Create date: 05, 12, 2024
--- Description: drop foregin key
+-- Description: Drop Foreign key
 -- ---------------------------------------*****************************************************--------------------------------------------------
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
 
 CREATE PROCEDURE [Project3].[DropForeignKeysFromQueensClassSchedule]
     @UserAuthorizationKey INT
 AS
 BEGIN
- 
-    IF EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[Course].[FK_Class_Instructor]') AND parent_object_id = OBJECT_ID(N'[Course].[Class]'))
-        ALTER TABLE [Course].[Class] DROP CONSTRAINT [FK_Class_Instructor];
+    ALTER TABLE [Course].[Class]
+    DROP CONSTRAINT IF EXISTS InstructorID, FK_Class_UserAuthorizationKey;
 
-    IF EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[Course].[FK_Class_UserAuthorization]') AND parent_object_id = OBJECT_ID(N'[Course].[Class]'))
-        ALTER TABLE [Course].[Class] DROP CONSTRAINT [FK_Class_UserAuthorization];
+    ALTER TABLE [Course].[Course]
+    DROP CONSTRAINT IF EXISTS  FK_Course_UserAuthorizationKey;
 
-    IF EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[Course].[FK_Course_UserAuthorization]') AND parent_object_id = OBJECT_ID(N'[Course].[Course]'))
-        ALTER TABLE [Course].[Course] DROP CONSTRAINT [FK_Course_UserAuthorization];
+    ALTER TABLE [Course].[Mode_Of_Instruction]
+    DROP CONSTRAINT IF EXISTS  FK_MOI_UserAuthorizationKey;
 
-    IF EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[Course].[FK_ModeOfInstruction_UserAuthorization]') AND parent_object_id = OBJECT_ID(N'[Course].[ModeOfInstruction]'))
-        ALTER TABLE [Course].[ModeOfInstruction] DROP CONSTRAINT [FK_ModeOfInstruction_UserAuthorization];
+    ALTER TABLE [Course].[CoursesClassMode]
+    DROP CONSTRAINT IF EXISTS  Classid, Modeid, Courseid, FK_CourseBridge_UserAuthorizationKey;
 
+    ALTER TABLE [Department].[Instructor]
+    DROP CONSTRAINT IF EXISTS  FK_Instructor_UserAuthorizationKey;
 
-    IF EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[Course].[FK_CourseClassMode_Course]') AND parent_object_id = OBJECT_ID(N'[Course].[CourseClassMode]'))
-        ALTER TABLE [Course].[CourseClassMode] DROP CONSTRAINT [FK_CourseClassMode_Course];
+    ALTER TABLE [Department].[Departments]
+    DROP CONSTRAINT IF EXISTS  FK_Department_UserAuthorizationKey;
 
-    IF EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[Course].[FK_CourseClassMode_ModeOfInstruction]') AND parent_object_id = OBJECT_ID(N'[Course].[CourseClassMode]'))
-        ALTER TABLE [Course].[CourseClassMode] DROP CONSTRAINT [FK_CourseClassMode_ModeOfInstruction];
+    ALTER TABLE [Department].[InstructorDepartment]
+    DROP CONSTRAINT IF EXISTS  InstructorID, DepartmentID, FK_DepBridge_UserAuthorizationKey;
 
-    IF EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[Course].[FK_CourseClassMode_Class]') AND parent_object_id = OBJECT_ID(N'[Course].[CourseClassMode]'))
-        ALTER TABLE [Course].[CourseClassMode] DROP CONSTRAINT [FK_CourseClassMode_Class];
+    ALTER TABLE [Location].[BuildingLocation]
+    DROP CONSTRAINT IF EXISTS Classid, FK_Building_UserAuthorizationKey;
 
-    IF EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[Course].[FK_CourseClassMode_UserAuthorization]') AND parent_object_id = OBJECT_ID(N'[Course].[CourseClassMode]'))
-        ALTER TABLE [Course].[CourseClassMode] DROP CONSTRAINT [FK_CourseClassMode_UserAuthorization];
-
-
-    IF EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[Department].[FK_Instructor_UserAuthorization]') AND parent_object_id = OBJECT_ID(N'[Department].[Instructor]'))
-        ALTER TABLE [Department].[Instructor] DROP CONSTRAINT [FK_Instructor_UserAuthorization];
-
-
-    IF EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[Department].[FK_InstructorDepartment_UserAuthorization]') AND parent_object_id = OBJECT_ID(N'[Department].[InstructorDepartment]'))
-        ALTER TABLE [Department].[InstructorDepartment] DROP CONSTRAINT [FK_InstructorDepartment_UserAuthorization];
-
-
-    IF EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[Department].[FK_Department_UserAuthorization]') AND parent_object_id = OBJECT_ID(N'[Department].[Department]'))
-        ALTER TABLE [Department].[Department] DROP CONSTRAINT [FK_Department_UserAuthorization];
-
-
-    IF EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[Location].[FK_BuildingLocation_Class]') AND parent_object_id = OBJECT_ID(N'[Location].[BuildingLocation]'))
-        ALTER TABLE [Location].[BuildingLocation] DROP CONSTRAINT [FK_BuildingLocation_Class];
-
-    IF EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[Location].[FK_BuildingLocation_UserAuthorization]') AND parent_object_id = OBJECT_ID(N'[Location].[BuildingLocation]'))
-        ALTER TABLE [Location].[BuildingLocation] DROP CONSTRAINT [FK_BuildingLocation_UserAuthorization];
-
-
-    IF EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[Location].[FK_RoomLocation_BuildingLocation]') AND parent_object_id = OBJECT_ID(N'[Location].[RoomLocation]'))
-        ALTER TABLE [Location].[RoomLocation] DROP CONSTRAINT [FK_RoomLocation_BuildingLocation];
-
-    IF EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[Location].[FK_RoomLocation_UserAuthorization]') AND parent_object_id = OBJECT_ID(N'[Location].[RoomLocation]'))
-        ALTER TABLE [Location].[RoomLocation] DROP CONSTRAINT [FK_RoomLocation_UserAuthorization];
+    ALTER TABLE [Location].[RoomLocation]
+    Drop CONSTRAINT if EXISTS BuildingLocationID, FK_Room_UserAuthorizationKey
 
     -- now we just log the work :D
-    EXEC [Project3].[TrackWorkFlow] @WorkFlowStepDescription = 'Drop Foreign Keys from QueensClassSchedule DB', @UserAuthorizationKey = @UserAuthorizationKey;
+    EXEC [Project3].[TrackWorkFlow] @WorkFlowStepsDescription = 'Drop Foreign Keys from QueensClassSchedule DB',
+    @UserAuthorizationKey = @UserAuthorizationKey,
+    @WorkflowStepsTableRowCount = @@ROWCOUNT;
 END;
+GO
