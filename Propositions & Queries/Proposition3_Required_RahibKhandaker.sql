bb4e4a4a9ac7e7d61fd5fdc22d13bebb28ac2134
Rahib-Khan <rahib.khandaker@gmail.com>
2024-05-14 21:10:03 -0400

    Alias in prop 3

--- Propositions & Queries/Proposition3_Required_RahibKhandaker.sql
+++ Propositions & Queries/Proposition3_Required_RahibKhandaker.sql
@@ -4,7 +4,7 @@
 -- Description: Proposition #3 Required - How may classes that are being taught that semester grouped by course and aggregate the total enrollment, total class limit and the percentage of enrollment.
 -- ====================================================================================================================================================================================================
 
-select CourseCode, count(a.CourseId), sum(Enrollment), sum(Limit), (sum(Enrollment) * 100/ sum(Limit)) from Course.CoursesCLassMode a
+select CourseCode, count(a.CourseId) As ClassCountinCourse, sum(Enrollment) as TotalEnrollment, sum(Limit) as TotalLimit, (sum(Enrollment) * 100/ sum(Limit)) as EnrollPercent from Course.CoursesCLassMode a
 join Course.Class b on a.ClassID = b.Classid
 join Course.Course c on a.CourseId = c.Courseid
 group by a.CourseId,CourseCode
